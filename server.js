3dfd2d391b6f8ff0279e8614feafe51915301551
Saurabh kumar <saurabhkumar@Saurabhs-MacBook-Pro.local>
2026-02-18 18:18:20 +0530

    Fix Vercel deployment configuration

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
